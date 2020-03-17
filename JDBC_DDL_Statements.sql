92e6d74359f94680a14357717e189dec14c94421
sandra_c <chavezsandra0498@gmail.com>
2020-03-16 19:34:45 -0700

    Update JDBC_DDL_Statements.sql

--- JDBC_DDL_Statements.sql
+++ JDBC_DDL_Statements.sql
@@ -74,3 +74,23 @@ Update Books
 //Remove a book specified by the user
 Delete From Books
 	Where bookTitle = 'userInput';
+
+
+
+
+insert into Publishers(publishername,publisheraddress, publisherphone, publisheremail) VALUES
+('J.B Lippincott & Co.', '1234 Orange ave.', 5667890, 'JBWriting@yahoo.com'),
+('Little, Brown and Company', '4343 Main Street', 5553978, 'LittleGroup@yahoo.com'),
+('Scholastic', '568 Crescent Street', 5653145, 'ScholasticBusiness@gmail.com');
+
+
+insert into books(groupname, booktitle, publishername, yearpublished,numberpages ) VALUES 
+('Storytellers', 'To Kill a MockingBird', 'J.B Lippincott & Co.', 1960, 281),
+('Novice Narrators','The Catcher in the Rye', 'Little, Brown and Company',1951,277),
+('Goosebumps Writing Group','Welcome to Dead House', 'Scholastic', 1992, 128),
+('Goosebumps Writing Group', 'Return of the Mummy','Scholastic', 1994, 118);
+
+insert into WritingGroup(groupname, headwriter, yearformed, subject) VALUES 
+('Storytellers','Harper Lee', 1950, 'Morality'),
+('Novice Narrators','J.D Salinger', 1940, 'Alienation'),
+('Goosebumps Writing Group', 'R.L Stine', 1990, 'Horror');
